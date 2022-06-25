e7c98d4bf4e6a4fac23c1bc41ee6a359a98b30de
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-06-26 00:17:47 +0200

    Add project files.

---
+++ DiceRollerLogic/Dice.cs
@@ -0,0 +1,39 @@
+﻿namespace DiceRollerLogic
+{
+    public class Dice : IRoller
+    {
+        /// <summary>
+        /// Property returns maximum possible outcome of a dice roll.
+        /// </summary>
+        public int Range { get; protected set; }
+        /// <summary>
+        /// Property returns a name of a dice.
+        /// (dice name is connected to its range ex. d20, d6, d4, ...)
+        /// </summary>
+        public string Name { get; protected set; }
+        /// <summary>
+        /// Property returns description of visual pattern of the dice.
+        /// </summary>
+        public string Pattern { get; protected set; }
+        /// <summary>
+        /// Constructor for general dices.
+        /// </summary>
+        /// <param name="range"> Maximum outcome of a dice </param>
+        /// <param name="name"> Name of a dice </param>
+        /// <param name="pattern"> Description of visual pattern of the dice </param>
+        public Dice(int range, string name="d" , string pattern="default")
+        {
+            Range = range;
+            Name = name;
+            Pattern = pattern;
+        }
+        public int RollDice()
+        {
+            return IRoller.GetOutcome(Range);
+        }
+        public override string ToString()
+        {
+            return $"{Name} can roll values from 1 to {Range}";
+        }
+    }
+}
---
+++ DiceRollerLogic/IRoller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerLogic
+{
+    public interface IRoller
+    {
+        private static readonly Random randomizer = new Random();
+        /// <summary>
+        /// Method that returns outcome of a single dice roll.
+        /// </summary>
+        /// <param name="range"> Maximum value of a dice roll. </param>
+        /// <returns> Integer value containing outcome of the roll. </returns>
+        protected static int GetOutcome(int range)
+        {
+            return randomizer.Next(1, range+1);
+        }
+
+        public int RollDice();
+    }
+}
